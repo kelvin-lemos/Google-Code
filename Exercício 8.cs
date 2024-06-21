b93006a2d4771f67b62e384cd37af20dd37c3539
Kelvin Lemos dos Santos <166047629+kelvin-lemos@users.noreply.github.com>
2024-06-21 20:55:34 -0300

    Add files via upload

---
+++ Exercício 8.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_8_Exercício_8
+{
+    internal class Program
+    {
+        static string ObterRadical(string verbo)
+        {
+            return verbo.Substring(0, verbo.Length - 2);
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um verbo regular (termina com 'ar'): ");
+            string verbo = Console.ReadLine();
+
+
+        }
+    }
+}
